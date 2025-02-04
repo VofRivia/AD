c5a025e499787e7cd334fa797a07273333f05348
VofRivia <123163492+VofRivia@users.noreply.github.com>
2025-02-04 14:01:55 +0200

    Add files via upload

--- jdbc/src/Main.java
+++ jdbc/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         // Class.forName("org.postgresql.Driver");   - Apparently the posgresql driver is loaded by default
         //                                              when importing java.sql making this line obsolete.
-        Class.forName("org.postgresql.Driver");
+
         String url = "jdbc:postgresql://localhost:5432/";
 
         Properties props = new Properties();
@@ -16,15 +16,17 @@ public class Main {
         Statement st = conn.createStatement();
         st.executeUpdate("CREATE TABLE IF NOT EXISTS personnes ( nom VARCHAR(50), age SMALLINT)");
         st.executeUpdate("TRUNCATE TABLE personnes");
-        // or
-        // st.executeUpdate("DROP TABLE IF EXISTS personnes");
-        // st.executeUpdate("CREATE TABLE personnes ( nom VARCHAR(50), age SMALLINT)");
+        /*
+         or
+         st.executeUpdate("DROP TABLE IF EXISTS personnes");
+         st.executeUpdate("CREATE TABLE personnes ( nom VARCHAR(50), age SMALLINT)");
+        */
 
         st.executeUpdate("INSERT INTO personnes VALUES ('Joe', 24)");
         st.executeUpdate("INSERT INTO personnes VALUES ('Valerie', 27)");
         st.executeUpdate("INSERT INTO personnes VALUES ('Carl', 26)");
         st.executeUpdate("INSERT INTO personnes VALUES ('Lettie', 26)");
-        
+
         ResultSet rs = st.executeQuery( "SELECT * FROM personnes" );
         while (rs.next()) {
             String nom = rs.getString("nom");
